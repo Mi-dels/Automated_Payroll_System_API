decbc62b044add18ad679012b7a05073a77bf2af
Mi-dels <lamyogunjobi85@gmail.com>
2026-05-03 12:42:19 -0700

    tenth commit

--- automated_payroll_system/settings.py
+++ automated_payroll_system/settings.py
@@ -258,50 +258,50 @@ REST_FRAMEWORK = {
 #     "SERVE_INCLUDE_SCHEMA": False,
 # }
 
-SPECTACULAR_SETTINGS = {
-    "TITLE": "Payroll API",
+# SPECTACULAR_SETTINGS = {
+#     "TITLE": "Payroll API",
 
-    "DESCRIPTION": "HR + Payroll System API",
+#     "DESCRIPTION": "HR + Payroll System API",
 
-    "VERSION": "1.0.0",
+#     "VERSION": "1.0.0",
 
-    "SERVE_INCLUDE_SCHEMA": False,
+#     "SERVE_INCLUDE_SCHEMA": False,
 
-    "SWAGGER_UI_SETTINGS": {
-        "persistAuthorization": True,
-    },
+#     "SWAGGER_UI_SETTINGS": {
+#         "persistAuthorization": True,
+#     },
 
-    "SECURITY": [
-        {
-            "BearerAuth": []
-        }
-    ],
+#     "SECURITY": [
+#         {
+#             "BearerAuth": []
+#         }
+#     ],
 
-    "COMPONENT_SPLIT_REQUEST": True,
-}
-
-
-from drf_spectacular.extensions import (
-    OpenApiAuthenticationExtension
-)
-
-class BearerScheme(OpenApiAuthenticationExtension):
-
-    target_class = (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
-    )
-
-    name = "BearerAuth"
-
-    def get_security_definition(self, auto_schema):
-
-        return {
+#     "COMPONENT_SPLIT_REQUEST": True,
+# }
 
 
-            "type": "http",
-            "scheme": "bearer",
-            "bearerFormat": "JWT",
-        }
+# from drf_spectacular.extensions import (
+#     OpenApiAuthenticationExtension
+# )
+
+# class BearerScheme(OpenApiAuthenticationExtension):
+
+#     target_class = (
+#         "rest_framework_simplejwt.authentication.JWTAuthentication"
+#     )
+
+#     name = "BearerAuth"
+
+#     def get_security_definition(self, auto_schema):
+
+#         return {
+
+
+#             "type": "http",
+#             "scheme": "bearer",
+#             "bearerFormat": "JWT",
+#         }
 
 
 
@@ -328,6 +328,10 @@ SPECTACULAR_SETTINGS = {
            "bearerFormat": "JWT",
         }
     },
+
+    "SECURITY":[
+        {"BearerAuth": []}
+    ],
     
 }
 
