6c0776cddb2c586407b24de544d8b6db9017c257
Mi-dels <lamyogunjobi85@gmail.com>
2026-05-28 07:18:55 -0700

    minor mistakes

--- payroll/management/commands/createsu.py
+++ payroll/management/commands/createsu.py
@@ -5,8 +5,8 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         User = get_user_model
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
+        if not User.object.filter(username='admin').exists():
+            User.object.create_superuser(
                 username='admin',
                 email='admin@gmail.com',
                 password='admiin1234'
