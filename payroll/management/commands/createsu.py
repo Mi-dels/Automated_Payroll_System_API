33389ca88fbcc512523d84270150d04a6cfe2bff
Mi-dels <lamyogunjobi85@gmail.com>
2026-05-28 07:04:48 -0700

    added an automatic superuser creation

---
+++ payroll/management/commands/createsu.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        User = get_user_model
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@gmail.com',
+                password='admiin1234'
+            )
+            self.stdout.write("Superuser created succesfully")
+        else:
+            self.stdout.write("Superuser already exists")
