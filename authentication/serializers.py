22fb846a69912ddc1772d3b79801471113aa2eaa
Mi-dels <lamyogunjobi85@gmail.com>
2026-05-29 04:38:43 -0700

    still fixing the issue

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -40,34 +40,35 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = User.objects.filter(username=data["username"]).first
-        # user = authenticate(
-        #     username=data["username"],
-        #     password=data["password"]
-        # )
-        # user = User.objects.filter(username=data["username"]).first()
+        try:
+            user = User.objects.filter(username=data["username"]).first
+        
 
-        if not user :
-            raise serializers.ValidationError(f"user not found{data['username']}")
-        if not user.check_password(data["password"]):
-            raise serializers.ValidationError(f"wrong password for user:{data['username']}")
+            if not user :
+                raise serializers.ValidationError(f"user not found{data['username']}")
+            if not user.check_password(data["password"]):
+                raise serializers.ValidationError(f"wrong password for user:{data['username']}")
 
-        refresh = RefreshToken.for_user(user)
+            refresh = RefreshToken.for_user(user)
 
-        return {
-            "user": {
-                "id": user.id,
-                "username": user.username,
-                "email": user.email,
-                "role": user.role,
-                "is_hr": user.is_hr,
-                "employee_id": user.employee_id,
-            },
-            "tokens": {
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-            },
-        }
+            return {
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "role": user.role,
+                    "is_hr": user.is_hr,
+                    "employee_id": user.employee_id,
+                },
+                "tokens": {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+            }
+        except serializers.ValidationError:
+            raise
+        except Exception as e:
+            raise serializers.ValidationError(f"login error: {str(e)}")    
 
 
 
